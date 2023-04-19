494b4f91c50743687e47261363f8b4442372173e
DLellep <david-andero.lellep@voco.ee>
2023-04-19 23:40:10 +0300

    Created splash screen

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -14,4 +14,5 @@ const Button = ({ title }) => {
       </TouchableOpacity>
   )
 }
+
 export default Button;
--- src/components/styles.js
+++ src/components/styles.js
@@ -15,4 +15,5 @@ export const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold"
   }
+  
 })
